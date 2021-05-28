baa760587ad3c0c924ef197899d53897dec69042
Martín Méndez <cp202136@zx81.famaf.unc.edu.ar>
2021-05-28 16:21:04 -0300

    OpenMP_v2.

--- lab3/solution8/run.sh
+++ lab3/solution8/run.sh
@@ -14,5 +14,8 @@ echo ++++++++++++++++++ FOR $num_threads THREADS ++++++++++++++++++
 echo #
 # +++ Ejecuto análisis de performance en x corridas y guardo los datos
 perf stat -e cpu-clock,cpu-cycles,instructions,cache-references,cache-misses -r 20 ./tiny_mc >> results.dat
-# +++ Muestro los datos en salida estandar por terminal
-#cat results.dat
+echo #
+echo ++++++++++++++++++       RESULTADOS         ++++++++++++++++++
+echo #
+# +++ Muestro los resultados por salida estandar
+cat results.dat
